da464eb496f644778244b06b6df43ec35c39c502
Rod Mac <rodmacbr@yahoo.com.br>
2023-06-28 16:45:35 +0100

    Refactor: Template list page; HTTP API calls centralized.

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,26 +1,38 @@
-import React from "react";
-import Header from "../components/headerMovieList";
-import Grid from "@mui/material/Grid";
-import MovieList from "../components/movieList";
+import React, { useState, useEffect } from "react";
+import PageTemplate from '../components/templateMovieListPage'
 
-const styles = {
-  root: {
-    padding: "20px",
-  },
-};
+const HomePage = (props) => {
+  const [movies, setMovies] = useState([]);
+  const favourites = movies.filter(m => m.favourite)
+  localStorage.setItem('favourites', JSON.stringify(favourites))
 
-const MovieListPage = (props) => {
-  const movies = props.movies;
+  const addToFavourites = (movieId) => {
+    const updatedMovies = movies.map((m) =>
+      m.id === movieId ? { ...m, favourite: true } : m
+    );
+    setMovies(updatedMovies);
+  };
+
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${import.meta.env.VITE_TMDB_KEY}&language=en-US&include_adult=false&page=1`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        return json.results;
+      })
+      .then((movies) => {
+        setMovies(movies);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
-    <Grid container sx={styles.root}>
-      <Grid item xs={12}>
-        <Header title={"Home Page"} />
-      </Grid>
-      <Grid item container spacing={5}>
-        <MovieList movies={movies}></MovieList>
-      </Grid>
-    </Grid>
+    <PageTemplate
+      title='Discover Movies'
+      movies={movies}
+      selectFavourite={addToFavourites}
+    />
   );
 };
-export default MovieListPage;
+export default HomePage;
