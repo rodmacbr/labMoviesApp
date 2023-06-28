b93388f483284e807a243522dacbebcf83413285
Rod Mac <rodmacbr@yahoo.com.br>
2023-06-28 16:59:06 +0100

    Exercise - Show favourite icon on Movie Details page..

--- src/components/headerMovie/index.jsx
+++ src/components/headerMovie/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import Paper from "@mui/material/Paper";
@@ -7,7 +7,7 @@ import Typography from "@mui/material/Typography";
 import HomeIcon from "@mui/icons-material/Home";
 
 const styles = {
-    root: {  
+  root: {
     display: "flex",
     justifyContent: "space-around",
     alignItems: "center",
@@ -18,6 +18,19 @@ const styles = {
 
 const MovieHeader = (props) => {
   const movie = props.movie;
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  useEffect(() => {
+    // Get favorites from localStorage
+    const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
+
+    // Check if current movie is a favorite
+    const isCurrentMovieFavorite = favorites.some(
+      (favMovie) => favMovie.id === movie.id
+    );
+
+    setIsFavorite(isCurrentMovieFavorite);
+  }, [movie.id]);
 
   return (
     <Paper component="div" sx={styles.root}>
@@ -28,11 +41,19 @@ const MovieHeader = (props) => {
       <Typography variant="h4" component="h3">
         {movie.title}{"   "}
         <a href={movie.homepage}>
-          <HomeIcon color="primary"  fontSize="='large"/>
+          <HomeIcon color="primary" fontSize="large" />
         </a>
         <br />
         <span>{`${movie.tagline}`} </span>
       </Typography>
+
+      {/* Render the red heart icon if the movie is a favorite */}
+      {isFavorite && (
+        <IconButton aria-label="favorite">
+          <FavoriteIcon color="error" fontSize="large" />
+        </IconButton>
+      )}
+
       <IconButton aria-label="go forward">
         <ArrowForwardIcon color="primary" fontSize="large" />
       </IconButton>
@@ -41,3 +62,4 @@ const MovieHeader = (props) => {
 };
 
 export default MovieHeader;
+
